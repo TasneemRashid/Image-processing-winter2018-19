853371101a1bba883be74d052f3b4bb91f6fe7dd
mehedihasan619 <mehedi.hasan619@gmail.com>
2018-12-15 15:21:51 +0100

    Task 2.1

--- Project_2/task2_1.py
+++ Project_2/task2_1.py
@@ -3,6 +3,7 @@ import scipy.ndimage as ndImage
 from scipy.signal import fftconvolve
 from matplotlib import pyplot as plt
 import cv2
+import timeit, functools
 from astropy.convolution import Gaussian2DKernel
 
 myImg = cv2.imread('bauckhage.jpg',0)                           #Read image and convert into float
@@ -12,7 +13,8 @@ cat = np.float32(cat_img)
 clock_img = cv2.imread('clock.jpg',0)
 clock = np.float32(clock_img)
 
-plt.figure()
+fig=plt.figure()
+fig.suptitle('Naive Implementation',fontsize=16)
 plt.subplot(1,2,1),plt.imshow(img,cmap = 'gray')
 plt.title("Original Image"), plt.xticks([]),                    #Plot the orginal Image
 plt.yticks([])
@@ -30,7 +32,8 @@ plt.yticks([])
 
 #task 2.1.1 Scipy Implementation
 position=1
-plt.figure()
+fig=plt.figure()
+fig.suptitle('SciPy Library',fontsize=16)
 for i in [3,5,9,15,23,33]:                                      #Array of filter size                                    
     sigma = (i-1.) / (2*2.575)
     ndarray = ndImage.gaussian_filter(img, 
@@ -38,16 +41,19 @@ for i in [3,5,9,15,23,33]:                                      #Array of filter
                                   order =0,mode='reflect',
                                   cval=0.0,truncate=4.0)        #Call gaussian_filter with parameters
     plt.subplot(2,3,position),plt.imshow(ndarray,cmap = 'gray') #Plot the Image 
-    plt.title("SciPy, "+str(i)+ 'x'+str(i)), plt.xticks([]),plt.yticks([])               
+    plt.title(str(i)+ 'x'+str(i)), plt.xticks([]),plt.yticks([])               
     position=position+1
 
 #task 2.1.1 CV2 Implementation
-filter_size =15   
-blur = cv2.GaussianBlur(img,(filter_size,filter_size),0)        #Call GaussianBlue with filter size
+filter_size =15
+def cv2GaussianBlur(filter_size):
+    blur = cv2.GaussianBlur(img,(filter_size,filter_size),0)    #Call GaussianBlue with filter size
+    return blur
+     
 plt.figure()
 plt.title("CV2 Implementation, "+str(filter_size)+'x'+str(filter_size)),
 plt.xticks([]), plt.yticks([]),                                 #Plot the Image 
-plt.imshow(blur, cmap = 'gray')                                 
+plt.imshow(cv2GaussianBlur(filter_size), cmap = 'gray')                                 
 
 #task 2.1.2
 sigma = 10.                                                     #Calculate weights g
@@ -84,7 +90,8 @@ oneDConvColumn=ndImage.gaussian_filter1d(columnConvolution,
                                          truncate =4.0)
 #h = rowConvolution*columnConvolution
 
-plt.figure()
+fig=plt.figure()
+fig.suptitle('Convolution',fontsize=16)
 plt.subplot(1,2,1),plt.imshow(rowConvolution,cmap = 'gray')        #Plot the Image 
 plt.title("Row Covolution "), plt.xticks([]),plt.yticks([]) 
 plt.subplot(1,2,2),plt.imshow(columnConvolution,cmap = 'gray')     #Plot the Image 
@@ -96,7 +103,8 @@ fft_of_img = np.fft.fft2(img)
 fShift_of_img = np.fft.fftshift(fft_of_img)                     #move zero frequency to the center of the spectrum
 absValue_of_img = np.log10(abs(fShift_of_img))
 
-plt.figure()
+fig=plt.figure()
+fig.suptitle('Convolution in Space Domain',fontsize=16)
 plt.subplot(2,3,1),plt.imshow(img,cmap = 'gray')                #Plot the Image 
 plt.title("Original Image"), plt.xticks([]),plt.yticks([])
 plt.subplot(2,3,2),plt.imshow(absValue_of_img,cmap = 'gray')    #Plot the Image 
@@ -121,6 +129,34 @@ plt.subplot(2,3,4),plt.imshow(absValue_of_gauss_kernel,cmap = 'gray')    #Plot t
 plt.title("FFT Filter"), plt.xticks([]),plt.yticks([])
 plt.subplot(2,3,5),plt.imshow(K,cmap = 'gray')                           #Plot the Image 
 plt.title("Multiplication FFT"), plt.xticks([]),plt.yticks([])
+
+#Task 2.1.4
+def g_sci(mask):
+   ndImage.gaussian_filter(clock, mask)
+   
+#Calculate runtime for OpenCV implementation
+arrayOne = []    
+for i in [3,5,9,15,23]:  
+    t = timeit.Timer(functools.partial(cv2GaussianBlur, i)).repeat(1, 1000)
+    t = np.array(t)
+    arrayOne = np.append(arrayOne,np.mean(t))
+    
+y = np.array([3, 5, 9, 15, 23])
+
+#Calculate for SciPy Implementation
+arrayTwo = []
+for i in [3,5,9,15,23]:
+    t = timeit.Timer(functools.partial(g_sci, i)).repeat(1, 1000)
+    t = np.array(t)
+    arrayTwo = np.append(arrayTwo,np.mean(t))
+
+plt.figure()
+plt.plot(arrayOne, y)
+plt.plot(arrayTwo, y)
+plt.legend(('OpenCV', 'SciPy'), loc='upper right')
+plt.xlabel('Run-times')
+plt.ylabel('Filter size')
+plt.title('Runtime of different approaches')
 plt.show()
 
 
