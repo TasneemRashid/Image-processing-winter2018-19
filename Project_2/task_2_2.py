2bf2ea7ecc8794a58359afcc15496a479e3ccc04
mehedihasan619 <mehedi.hasan619@gmail.com>
2018-12-16 20:58:26 +0100

    Task 2.2

--- Project_2/task_2_2.py
+++ Project_2/task_2_2.py
@@ -8,9 +8,9 @@ img2 = cv2.imread('clock.jpg',0)
 #Loop through the array of Filter Size
 position = 1 
 # Create a figure
-plt.figure()
+fig = plt.figure()
+fig.suptitle('Sobel',fontsize=16) 
 for i in [3,5,9,15]:
-    #laplacian = cv2.Laplacian(img1,cv2.CV_64F)
     sobelx = cv2.Sobel(img1,cv2.CV_64F,1,0,ksize=i)
     sobely = cv2.Sobel(img1,cv2.CV_64F,0,1,ksize=i)
     plt.subplot(2,4,position),plt.imshow(sobelx,cmap = 'gray')
@@ -18,13 +18,23 @@ for i in [3,5,9,15]:
     position=position+1  
     plt.subplot(2,4,position),plt.imshow(sobely,cmap = 'gray')
     plt.title('Size: '+ str(i)+ ', Y'), plt.xticks([]), plt.yticks([])
-    position=position+1     
+    position=position+1
+
+position =1
+fig = plt.figure()
+fig.suptitle('Laplacian',fontsize=16)    
+for i in [3,5,9,15]:
+    laplacian = cv2.Laplacian(img1,cv2.CV_64F)
+    plt.subplot(2,2,position),plt.imshow(laplacian,cmap = 'gray')
+    plt.title('Size: '), plt.xticks([]), plt.yticks([])
+    position=position + 1  
 
 def gradientFunction(img,mode,sigma):
     g = ndimg.filters.gaussian_gradient_magnitude(img, sigma=sigma,mode=mode, cval=0.0)
     return g
 
-plt.figure()
+fig = plt.figure()
+fig.suptitle('Gradient Magnitude',fontsize=16) 
 plt.subplot(2,3,1),plt.imshow(gradientFunction(img1,'reflect',5),cmap = 'gray')
 plt.title('Sigma 5'), plt.xticks([]), plt.yticks([])
 plt.subplot(2,3,2),plt.imshow(gradientFunction(img1,'reflect',10),cmap = 'gray')
